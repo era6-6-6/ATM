dda1c88eca211290543a483035d8f63c982d431a
martin šindler <sindler.martin1@email.cz>
2021-09-23 22:54:18 +0200

    added GetTransaction

--- ATMforms/Managers/HistoryManager.cs
+++ ATMforms/Managers/HistoryManager.cs
@@ -5,12 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ATMforms.Managers
 {
     class HistoryManager
     {
         private readonly string PATH = $@"{Environment.CurrentDirectory}/SmallDB/Transactions.txt";
+        public static List<string> Data;
+    
         public void GetTransaction(int id)
         { 
             if(!File.Exists(PATH))
@@ -18,12 +21,24 @@ namespace ATMforms.Managers
                var file = File.Create(PATH);
                file.Close();
             }
+            Data = new List<string>();
+            string text;
             using (StreamReader reader = File.OpenText(PATH))
             {
-                string text = reader.ReadToEnd();
-                Match Balance = Regex.Match(text, "");
+                 text = reader.ReadToEnd();
+}
+            Match Balance = Regex.Match(text, $"!(.*) , {id} , (,*) , (.*)!");
+            
+            for (int i = 0; i < Balance.Groups.Count; i++)
+            {
+                Data.Add($"{Balance.Groups[1]} - {Balance.Groups[2]} - {Balance.Groups[3]}");
+                Balance = Balance.NextMatch();
+                
             }
-
+            foreach (var item in Data)
+            {
+                Console.WriteLine(item);
+            }
         }
         public void SaveTransaction(DateTime date, int IDFrom , int IDTo, double amount)
         {
@@ -35,7 +50,7 @@ namespace ATMforms.Managers
             
             using(StreamWriter writer = File.AppendText(PATH))
             {
-                writer.WriteLine($"!{date} , {IDFrom} ,  {IDTo} , {amount}");
+                writer.WriteLine($"!{date} , {IDFrom} ,  {IDTo} , {amount}!");
             }
         }
     }
